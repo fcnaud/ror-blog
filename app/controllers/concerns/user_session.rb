64d9d31578cb5a75c0d8aaafd010e407a9a81722
fcnaud <dcf_xyz@outlook.com>
2017-08-10 14:01:44 +0800

    modify

--- app/controllers/concerns/user_session.rb
+++ app/controllers/concerns/user_session.rb
@@ -29,4 +29,5 @@ module Concerns
     end
 
   end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
-  validates :title, presence: true, length: {minimum: 5}
-
   belongs_to :user
+
+  validates :title, presence: true
+
 end
